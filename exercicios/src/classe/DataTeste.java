6435aab0748b8e83b98dbceed565cab462344c6e
Carlos Augusto Dias <carlos_augusto_1411@hotmail.com>
2021-12-12 20:53:20 -0300

    Java

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -12,6 +12,7 @@ public class DataTeste {
 		d2.mes=6;
 		d2.ano=1991;
 		
+//		Código comentado para uma versão aprimorada
 //		System.out.printf("No dia %d/%d/%d e no dia %d/%d/%d \nduas almas gemeas nasceram", 
 //				d1.dia, d1.mes, d1.ano ,d2.dia, d2.mes, d2.ano);
 		System.out.printf("No dia %s e no dia %s \nduas almas gemeas nasceram", d1.dataFormatada(),d2.dataFormatada());
